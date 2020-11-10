3ace1c6e058fdd3799b4d1a57a16c66403cd4a15
rabaarabaa <27743418+rabaarabaa@users.noreply.github.com>
2020-11-10 16:50:21 -0300

    parsers/__init__.py: Fix typo that made load_lark_grammar not look filename in the grammars folder.

--- version/parsers/__init__.py
+++ version/parsers/__init__.py
@@ -1,15 +1,15 @@
 import os
 
-from lark.lark import Lark
+from lark import Lark
 
-PARSERS_FOLDER = os.path.dirname(__file__)
+PARSERS_FOLDER = os.path.abspath(os.path.dirname(__file__))
 LARK_GRAMMARS_FOLDER = os.path.join(PARSERS_FOLDER, 'grammars')
 
 
-def load_lark_grammar(filename, **options):
+def load_lark_grammar(filename: str, **options) -> Lark:
     fp = os.path.join(LARK_GRAMMARS_FOLDER, filename)
     if not os.path.isfile(fp):
         raise FileNotFoundError("Grammar '{}' does not exist in version/parsers/grammars folder.".format(filename))
 
-    with open(filename) as f:
+    with open(fp) as f:
         return Lark(f, **options)
